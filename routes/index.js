5c9191ae6e9ef03c54f6089fb9701b9f65e9da3b
ORSHIH71 <Lustifer041571>
2020-01-18 11:18:54 -0600

    HW 5

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const superagent = require('superagent');
 const {map} = require('rxjs/operator');
-const url = require('url');
 const router = express.Router();
 
 async function findUser(id){
@@ -10,13 +9,17 @@ async function findUser(id){
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-	const {user} = url.parse(req.url, true).query;
+	const {user, page} = req.query;
 	try {
 		const result = await superagent
 			.get("https://randomuser.me/api/")
 			.query({results: user});
 		const {results} = result.body;
 		let users = results.map(r=>({firstname: r.name.first, lastname: r.name.last}));
+		res.links({
+			next: "http://localhost:8080/?page="+(parseInt(page)+1)+"&user="+user,
+			last: "http://localhost:8080/?page=10&user="+user
+		});
 		res.removeHeader("X-Powered-By");
 		res.json(users);
 	}
